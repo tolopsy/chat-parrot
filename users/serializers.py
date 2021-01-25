3e9cde238c14cab51d2a0ebd484d00d215b0ebc0
Tolulope <lanre.toluwa@gmail.com>
2021-01-25 20:41:39 +0100

    clean: exclude password in UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -18,7 +18,7 @@ class RefreshSerializer(serializers.Serializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        exclude = ("password", )
 
 
 class UserProfileSerializer(serializers.ModelSerializer):

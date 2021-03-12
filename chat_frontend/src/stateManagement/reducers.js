3c19a59580cf9630d2bc62ca851d391366cfbe65
Tolulope <lanre.toluwa@gmail.com>
2021-03-12 15:53:39 +0100

    state management

---
+++ chat_frontend/src/stateManagement/reducers.js
@@ -0,0 +1,13 @@
+const { updateChatAction } = require("./actions");
+
+export const updateChatState = {
+    chatState = false,
+};
+
+export const updateChatReducer = (state,  action) => {
+     if(action.type === "updateChatAction") {
+         return {...state, chatState: action.payload};
+     } else {
+         return state
+     }
+ };
---
+++ chat_frontend/src/stateManagement/store.js
@@ -0,0 +1,24 @@
+import React, { createContext, useReducer } from 'react';
+import { updateChatState } from "./reducers"
+import { updateChatReducer } from "./reducers"
+
+const reduceReducers = (...reducers) => (prevState, value, ...args) => {
+    reducers.reduce(
+        (newState, reducer) => reducer(newState, value, ...args),
+        prevState
+    );
+};
+
+const combinedReducers = reduceReducers(...updateChatReducer);
+const initialState = {...updateChatState};
+
+const store = createContext();
+const {Provider} = store;
+
+const StoreProvider = ({children}) => {
+    const [state, dispatch] =  useReducer(combinedReducers, initialState);
+
+    return <Provider value={ {state, dispatch} }>{children}</Provider>
+};
+
+export {store, StoreProvider};

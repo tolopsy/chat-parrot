5befeff0d8f3a673c93289f650bfe324adbb92be
Tolulope <lanre.toluwa@gmail.com>
2021-03-12 15:03:28 +0100

    props and state demo

--- chat_frontend/src/app.js
+++ chat_frontend/src/app.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function App() {
-    return <h2>Chat Parrot is built with React, Node and Django</h2>
+const App = () => {
+    const [valA, setValA] =  useState("");
+    const [valB, setValB] = useState("");
+
+    return (
+        <div>
+            <input value={valA} onChange={(e) => {
+                setValA(e.target.value);
+            }} />
+
+            <input value={valB} onChange={(e) => {
+                setValB(e.target.value);
+            }} />
+
+            <Result vA={valA} vB={valB} />
+        </div>
+    );
 }
 
 export default App;
+
+const Result = (props) => {
+    const [result, setResult] = useState("")
+    return (
+        <div>
+            <h4>Result: {result}</h4>
+
+            <button onClick={() => {
+                var finalVal = parseFloat(props.vA) + parseFloat(props.vB);
+                setResult(finalVal);
+            }}>
+                Calculate
+            </button>
+        </div>
+    )
+}
